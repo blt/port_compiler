a26b93afa64b28492bc01b4d9187db30a830f3bf
Chris McCord <chris@chrismccord.com>
2017-01-19 17:32:02 -0500

    Properly escape paths
    Project names contain spaces would only the first unbroken segment
    of the target path to be passed to the underlying compiler.

--- src/pc_port_specs.erl
+++ src/pc_port_specs.erl
@@ -131,7 +131,7 @@ get_port_spec(Config, OsType, {Arch, Target, Sources}) ->
     get_port_spec(Config, OsType, {Arch, Target, Sources, []});
 get_port_spec(Config, OsType, {_Arch, Target, Sources, Opts}) ->
     SourceFiles = lists:flatmap(fun(Source) ->
-                                        Source1 = filename:join(rebar_state:dir(Config), Source),
+                                        Source1 = escape_path(filename:join(rebar_state:dir(Config), Source)),
                                         filelib:wildcard(Source1)
                                 end, Sources),
     LinkLang =
@@ -186,3 +186,6 @@ port_opt(State, {env, Env}) ->
     {env, PortEnv};
 port_opt(_State, Opt) ->
     Opt.
+
+escape_path(Str) ->
+    re:replace(Str, "([ ()?])", "\\\\&", [global, {return, list}]).
