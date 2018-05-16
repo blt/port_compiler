b67948ea33a9b758215d5c8323a42c1c226aeb93
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2018-05-16 08:49:24 +0000

    Redo #37
    Instead of passing on the source spec as is to CC/CXX and provoke a
    build failure, we warn when no files were found.

--- src/pc_port_specs.erl
+++ src/pc_port_specs.erl
@@ -119,9 +119,23 @@ port_spec_from_legacy(Config) ->
             objects = port_objects(Sources),
             opts    = [port_opt(Config, O) || O <- fill_in_defaults([])]}.
 
+maybe_report_wc(Source, Found) ->
+    case Found of
+        [] ->
+            rebar_api:warn("Found no source files for:~n~p", [Source]);
+        _ ->
+            ok
+    end,
+    Found.
+
+wildcard(Source) ->
+    maybe_report_wc(Source, filelib:wildcard(Source)).
+wildcard(Source, WorkDir) ->
+    maybe_report_wc(Source, filelib:wildcard(Source, WorkDir)).
+
 port_sources(WorkDir, Sources) ->
     lists:flatmap(fun (Source) ->
-                      filelib:wildcard(Source, WorkDir)
+                      wildcard(Source, WorkDir)
                   end, Sources).
 
 maybe_switch_extension({win32, nt}, Target) ->
@@ -151,7 +165,7 @@ get_port_spec(Config, OsType, {_Arch, Target, Sources, Opts}) ->
                   Source1 = expand_env(Source, Env),
                   Source2 = rebar_utils:escape_chars(
                               filename:join(rebar_state:dir(Config), Source1)),
-                  filelib:wildcard(Source2)
+                  wildcard(Source2)
           end, Sources),
     LinkLang =
         case lists:any(
